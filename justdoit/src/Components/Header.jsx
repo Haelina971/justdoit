3a77eea2cb5ffab6edcf8136576fba440cf65242
Haelina <tarany31@gmail.com>
2023-02-10 20:27:38 -0600

    Added logo and set size using bootstrap

--- justdoit/src/Components/Header.jsx
+++ justdoit/src/Components/Header.jsx
@@ -1,8 +1,10 @@
 import NavBar from "./NavBar";
+import logo from "../assets/logo.png"
 
 function Header() {
     return (
         <div>
+            <img src={logo} className="h-25 d-inline-block" style={{width: 100}}/>
             <NavBar/>
         </div>
     );
