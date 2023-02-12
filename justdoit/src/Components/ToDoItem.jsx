4ac08e102833ee14c9e07c7e23b5072b8ef1bd9f
Haelina <tarany31@gmail.com>
2023-02-11 18:15:43 -0600

    Worked on the progress bar in ToDoItem Used react bootstrap

--- justdoit/src/Components/ToDoItem.jsx
+++ justdoit/src/Components/ToDoItem.jsx
@@ -1,8 +1,12 @@
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 function ToDoItem(props) {
+    //Renders one ToDoItem
+    //Use props from parent component ToDoList
     return (
         <div>
             <h2>{props.title}</h2>
-            <progress></progress>
+            <ProgressBar animated striped variant="success" now={10} />
             <div>{props.beginDate.toISOString()}</div>
         </div>
     );
