253b03cc0b134342eb2f1d1c9882bf1d2b838178
Haelina <tarany31@gmail.com>
2023-03-05 22:10:43 -0600

    Revert "Work on sign up form:" Revert because deleted ID form by mistake This reverts commit b3f7e4e82710c88c5a6ba3da9d6e94f0e08d1d3d.

--- justdoit/src/Components/Forms/IDForm.jsx
+++ justdoit/src/Components/Forms/IDForm.jsx
@@ -2,68 +2,26 @@ import Card from "react-bootstrap/Card";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Input from "./Input";
-import { useState } from "react";
 
 const IDForm = (props) => {
-  const [userInput, setUserInput] = useState(
-    {
-        enteredEmail: '',
-        enteredPassword: '',
-    }
-  );
-
-  //Change enteredEmail value 
-  const emailChangeHandler = (event) => {
-    setUserInput((prevState) => {
-        return {...prevState, enteredEmail: event.target.value};
-    });
-  }
-
-  //Change enteredPassword value
-  const passwordChangeHandler = (event) => {
-    setUserInput((prevState) => {
-        return {...prevState, enteredPassword: event.target.value};
-    });
-  }
-
-  //Collect data from the different inputs into object
-  //Reset value of input fields
-  const submitHandler = (event) => {
-    event.preventDefault();
-    const userData = {
-        email: userInput.enteredEmail,
-        password: userInput.enteredPassword,
-    }
-    //props.onSaveUserData(userData);
-    console.log(userData);
-    setUserInput({
-        enteredEmail:'',
-        enteredPassword: ''
-    })
-  };
-
   return (
     <Card className="m-5 p-2 w-25 mx-auto">
-      <Form className="m-3" onSubmit={submitHandler}>
+      <Form className="m-3">
         <Input
           controlId="formBasicEmail"
           name="Email address"
           type="email"
           placeholder="Enter email"
-          onChange={emailChangeHandler}
-          value={userInput.enteredEmail}
-          autoComplete="email"
+          onChange={props.onChange}
         />
         <Input
           controlId="formBasicPassword"
           type="password"
           placeholder="Password"
-          onChange={passwordChangeHandler}
-          value={userInput.enteredPassword}
-          autoComplete="password"
+          onChange={props.onChange}
         />
         <Button variant="success" className="my-3" type="submit">
-          Register
+          {props.action}
         </Button>
       </Form>
     </Card>
